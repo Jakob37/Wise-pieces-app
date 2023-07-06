8374b56bf911155f88eae680a00b4eeb47402a07
Jakob Willforss <jakob.willforss@hotmail.com>
2023-07-06 09:43:03 +0200

    Prettier

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, TextInput, View } from 'react-native'
 
 function App(): JSX.Element {
+  const [text, setText] = useState('')
+  const [entries, setEntries] = useState([])
+
   return (
     <View>
       <Text>Hello world!</Text>
+      <TextInput
+        style={{ borderWidth: 1, borderColor: 'white' }}
+        onChangeText={() => {
+          console.log('Changing text')
+        }}
+        value={text}></TextInput>
     </View>
-  );
+  )
 }
 
-export default App;
+export default App
